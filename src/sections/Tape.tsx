7dec3baaa4a5c8d3655bf55105309d348b926a7e
Ujjwal Sharma <ujjwalsh2004@gmail.com>
2025-11-22 00:17:20 +0530

    feat: Update TapeSection to display an additional word in the scrolling animation

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -21,7 +21,7 @@ export const TapeSection = () => {
             <div className="bg-gradient-to-r from-accent/10 to-violet-500/10 -rotate-3 -mx-20 border-y border-white/5 backdrop-blur-sm">
                 <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
                     <div className="flex flex-none gap-4 py-3 pr-4 animate-scroll-left [animation-duration:30s] hover:[animation-play-state:paused] select-none">
-                        {[...new Array(3)].fill(0).map((_, idx) => (
+                        {[...new Array(4)].fill(0).map((_, idx) => (
                             <Fragment key={idx}>
                                 {words.map((word) => (
                                     <div key={`${word}-${idx}`} className="inline-flex gap-4 items-center">
